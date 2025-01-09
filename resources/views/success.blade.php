b1ebbd61d4045ab6f11afd86a49cc4c41ada2cc8
PiotrSaw <piotr.sawecki1@gmail.com>
2025-01-09 23:10:35 +0100

    responsywność

--- resources/views/success.blade.php
+++ resources/views/success.blade.php
@@ -5,14 +5,19 @@
 <head>
     <title> Bike master </title>
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
-    <link rel="stylesheet" href="{{asset('css/form.css')}}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body>
-    @include('layouts.nav')
+    <div id="kontener">
+        <header>
+            <h1>Bike master</h1>
+        </header>
+        @include('layouts.nav')
 
-    <main>
-        <h2>Rezerwacja została pomyślnie dokonana!</h2>
-        <p>Twoja rezerwacja naprawy roweru została zapisana. Dziękujemy za skorzystanie z naszej usługi.</p>
-    </main>
+        <main style="background-color: #0a0a0a; padding: 20px; border-radius: 10px; margin-top: 20px;">
+            <h2>Rezerwacja została pomyślnie dokonana!</h2>
+            <p>Twoja rezerwacja naprawy roweru została zapisana. Dziękujemy za skorzystanie z naszej usługi.</p>
+        </main>
+    </div>
 </body>
